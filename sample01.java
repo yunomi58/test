0ad6a3c32ea3f8f87cea21d2ecd3a910d123ffcd
yunomi <yunomi.com>
2019-01-04 19:38:49 +0900

    add new file

---
+++ sample01.java
@@ -0,0 +1,7 @@
+public class Sample01{
+	public static void main(String[] args){
+		System.out.println("‰ïˆõ“o˜^:" + args[0]);		
+		System.out.println("–¼:" + args[1]);
+		System.out.println("”N—î:" + args[2]);
+	}
+}
