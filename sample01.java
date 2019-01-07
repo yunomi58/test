84fc616e886341af5c9adf27c8dce8b0f8fa0926
yunomi58 <yunomi.com>
2019-01-07 16:38:14 +0900

    初めてのGit

--- sample01.java
+++ sample01.java
@@ -1,7 +1,5 @@
 public class Sample01{
 	public static void main(String[] args){
-		System.out.println("‰ïˆõ“o˜^:" + args[0]);		
-		System.out.println("–¼:" + args[1]);
-		System.out.println("”N—î:" + args[2]);
+		System.out.println("HelloGit" + args);		
 	}
 }
